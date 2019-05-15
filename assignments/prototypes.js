628be70469cabf423080b300b5c4bd9c4eb2dfbb
Binish Ali <binish83@gmail.com>
2019-05-15 19:03:12 -0400

    Stretch my Ass

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -147,3 +147,26 @@ Humanoid.prototype.greet = function() {
   // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
   // * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
   // * Create two new objects, one a villain and one a hero and fight it out with methods!
+
+  //Villain Constructor
+  function Villian(Villainattributes){
+    this.okLooks = Villainattributes.okLooks;
+    this.HotHealth = Villainattributes.HotHealth;
+    Humanoid.call(this, Villainattributes); 
+  }
+
+  //Inheritance
+  Villian.prototype = Object.create(Humanoid.prototype); 
+
+  
+
+  //Hero Constructor
+  function Hero (HeroAttributes){
+    this.goodLooks = heroAttributes.goodlooks;
+    this.HotHealth = heroAttributes.HotHealth;
+    Humanoid.call(this, heroAttributes);
+  }
+
+  //Inheritance
+  Hero.prototype = Object.create(Humanoid.prototype);  
+  
